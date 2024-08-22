382c9c4f24c3bc13663395dda5f69a0799feed48
= <67224573+subhamgupta28@users.noreply.github.com>
2024-08-22 23:16:59 +0530

    prop update

--- src/main/java/dev/automata/automata/AutomataApplication.java
+++ src/main/java/dev/automata/automata/AutomataApplication.java
@@ -2,11 +2,12 @@ package dev.automata.automata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class AutomataApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AutomataApplication.class, args);
 
