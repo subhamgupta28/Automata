8351ee25ac790c64f953070634d3f8b439647bde
= <67224573+subhamgupta28@users.noreply.github.com>
2024-10-11 01:31:11 +0530

    changed / included frontend

--- frontend/src/services/apis.jsx
+++ frontend/src/services/apis.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/v1/main/';
+const BASE_URL = 'http://localhost:8010/api/v1/main/';
 
 
 export const getDevices = async () => {
