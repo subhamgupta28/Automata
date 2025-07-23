45775e362a5dd89e341a686ca9ad2c81c93cf585
= <shubhamgupta21020@gmail.com>
2025-07-23 16:45:00 +0530

    fixes

--- src/main/java/dev/automata/automata/configs/SecurityConfiguration.java
+++ src/main/java/dev/automata/automata/configs/SecurityConfiguration.java
@@ -61,6 +61,7 @@ public class SecurityConfiguration {
             "/actuator/metrics/**", // for root
             "/ws/**",
             "/ws",
+            "/exp",
             "/app/**",
             "/topic/**",
             "/queue/**",
