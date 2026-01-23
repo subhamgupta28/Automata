75ef898d00299855e86acec36ed9f7431f212163
Subham gupta <67224573+subhamgupta28@users.noreply.github.com>
2026-01-23 11:57:14 +0530

    new ui update

--- src/main/java/dev/automata/automata/service/NotificationService.java
+++ src/main/java/dev/automata/automata/service/NotificationService.java
@@ -81,7 +81,7 @@ public class NotificationService {
 //        headers.add("Tags", "warning");
 
             HttpEntity<String> request = new HttpEntity<>(message, headers);
-            restTemplate.postForObject("http://ntfy:82/automata", request, String.class);
+            restTemplate.postForObject("http://ntfy/automata", request, String.class);
             System.err.println("NTFY sent: " + message);
         } catch (Exception e) {
             System.err.println(e);
