0f3334a8475ff2fc1a4acabcf6b7f734616edd56
Subham gupta <67224573+subhamgupta28@users.noreply.github.com>
2026-04-07 02:38:36 +0530

    MAJOR UPDATE: ui fixes

--- frontend/src/components/action/ActionNode.jsx
+++ frontend/src/components/action/ActionNode.jsx
@@ -1,16 +1,28 @@
 import {Handle, Position, useNodeConnections, useReactFlow} from "@xyflow/react";
 import React, {useEffect, useState} from "react";
 import {useCachedDevices} from "../../services/AppCacheContext.jsx";
-import {Card, FormControl, InputLabel, MenuItem, Select, Switch, TextField} from "@mui/material";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Chip,
+    FormControl,
+    IconButton,
+    InputLabel,
+    MenuItem,
+    Select,
+    Switch,
+    TextField
+} from "@mui/material";
 import Typography from "@mui/material/Typography";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 
 const actionStyle = {
-    padding: '10px',
+    // padding: '10px',
     borderRadius: '10px',
-    width: '220px',
+    width: '320px',
     boxShadow: '0 1px 3px rgba(0, 0, 0, 0.1)',
     border: '2px solid #0288D1',
     background: 'transparent',
@@ -212,10 +224,8 @@ export const ActionNode = ({id, data, isConnectable}) => {
     }
 
     return (
-        <Card style={{
-            ...actionStyle,
-            border: `2px solid ${conditionGroup === 'negative' ? '#f44336' : conditionGroup === 'positive' ? '#4caf50' : '#FFF'}`,
-        }}>
+        <div>
+
             <Handle
                 style={{
                     width: '18px',
@@ -227,142 +237,148 @@ export const ActionNode = ({id, data, isConnectable}) => {
                 id="b"
                 isConnectable={isConnectable}
             />
-            <Typography variant="body1" fontWeight="bold" sx={{marginLeft: 1}}>
-                {conditionGroup === 'negative' ? 'Negative Action' : conditionGroup === 'positive' ? 'Positive Action' : 'Not Action'}
-            </Typography>
-            {/*<div style={{*/}
-            {/*    display: 'inline-block',*/}
-            {/*    // marginLeft: '8px',*/}
-            {/*    marginBottom: '4px',*/}
-            {/*    padding: '2px 8px',*/}
-            {/*    borderRadius: '4px',*/}
-            {/*    width: '100%',*/}
-            {/*    fontSize: '11px',*/}
-            {/*    fontWeight: 'bold',*/}
-            {/*    // background: conditionGroup === 'negative' ? '#f44336' : conditionGroup === 'positive' ? '#4caf50' : '#808080',*/}
-            {/*    color: conditionGroup === 'negative' ? '#f44336' : conditionGroup === 'positive' ? '#4caf50' : '#FFF',*/}
-            {/*    border: `1px solid ${conditionGroup === 'negative' ? '#f44336' : conditionGroup === 'positive' ? '#4caf50' : '#FFF'}`*/}
-            {/*}}>*/}
-
-            {/*</div>*/}
-            <div style={{margin: '2px'}}>
-                <IconButton onClick={() => deleteNode(id)} style={{position: 'absolute', top: '0', right: '0'}}>
-                    <DeleteIcon/>
+            <div style={{display: 'flex', justifyContent: 'center', gap: '6px', margin: '4px', alignItems: 'center'}}>
+                <Chip size="small" label={"Order: " + order}> </Chip>
+                <Chip size="small" label={"Delay Seconds: " + delaySeconds}> </Chip>
+                <IconButton onClick={() => deleteNode(id)}>
+                    <DeleteIcon sx={{color: '#ff0000'}}/>
                 </IconButton>
-                <FormControl fullWidth sx={{marginTop: 3}} className='nodrag'>
-                    <InputLabel>Trigger Device</InputLabel>
-                    <Select
-                        variant='outlined'
-                        size='small'
-                        value={selectedDevice.id}
-                        label="Action Device"
-                        onChange={(e) => selectDevice(e)}
-                        name="deviceId"
-                    >
-                        {devices && devices.map((device) => (
-                            <MenuItem key={device.name} value={device.id} width={100}>
-                                {device.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                <FormControl fullWidth sx={{marginBottom: 1, marginTop: 1}} className='nodrag'>
-                    <InputLabel>Trigger Key</InputLabel>
-                    <Select
-                        variant='outlined'
-                        size='small'
-                        value={key}
-                        label="Action Key"
-                        onChange={(e) => handleTriggerKey(e, 'key')}
-                        name="key"
-                    >
-                        {actionKeys && actionKeys.map((actionOption) => (
-                            <MenuItem key={actionOption.id} value={actionOption.key}>
-                                {actionOption.displayName}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                {actionType !== "" && valueOptions.length > 0 ? (
-                    <FormControl fullWidth sx={{marginBottom: 1, marginTop: 1}} className='nodrag'>
-                        <InputLabel>Choose value</InputLabel>
-                        <Select
-                            variant='outlined'
-                            size='small'
-                            value={value}
-                            label="Choose value"
-                            onChange={(e) => handleTriggerValue(e, 'value')}
-                            name="key"
-                        >
-                            {valueOptions && valueOptions.map((actionOption) => (
-                                <MenuItem key={actionOption.name} value={actionOption.value}>
-                                    {actionOption.name}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                ) : (
-                    actionType === "COLOR" ? (
-                        <input
-                            type="color"
-                            value={value}
-                            onChange={(e) => handleTriggerKey(e, 'data')}
-                            style={{
-                                width: '100%',
-                                height: '50px',
-                                border: "none",
-                                cursor: "pointer",
-                                background: "none",
-                            }}
-                        />
-                    ) : (
+            </div>
+            <Accordion style={{
+                borderRadius: '12px', marginTop: '0px', ...actionStyle,
+                border: `2px solid ${conditionGroup === 'negative' ? '#f44336' : conditionGroup === 'positive' ? '#4caf50' : '#FFF'}`,
+            }}>
+                <AccordionSummary
+                    expandIcon={<ArrowDownwardIcon/>}
+
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                    <Typography>
+                        {selectedDevice.name + " | " + actionKeys?.filter(f => f.key === key)[0]?.displayName + " | " + value}
+                    </Typography>
+
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div>
+
+                        <FormControl fullWidth sx={{marginTop: 3}} className='nodrag'>
+                            <InputLabel>Trigger Device</InputLabel>
+                            <Select
+                                variant='outlined'
+                                size='small'
+                                value={selectedDevice.id}
+                                label="Action Device"
+                                onChange={(e) => selectDevice(e)}
+                                name="deviceId"
+                            >
+                                {devices && devices.map((device) => (
+                                    <MenuItem key={device.name} value={device.id} width={100}>
+                                        {device.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <FormControl fullWidth sx={{marginBottom: 1, marginTop: 1}} className='nodrag'>
+                            <InputLabel>Trigger Key</InputLabel>
+                            <Select
+                                variant='outlined'
+                                size='small'
+                                value={key}
+                                label="Action Key"
+                                onChange={(e) => handleTriggerKey(e, 'key')}
+                                name="key"
+                            >
+                                {actionKeys && actionKeys.map((actionOption) => (
+                                    <MenuItem key={actionOption.id} value={actionOption.key}>
+                                        {actionOption.displayName}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        {actionType !== "" && valueOptions.length > 0 ? (
+                            <FormControl fullWidth sx={{marginBottom: 1, marginTop: 1}} className='nodrag'>
+                                <InputLabel>Choose value</InputLabel>
+                                <Select
+                                    variant='outlined'
+                                    size='small'
+                                    value={value}
+                                    label="Choose value"
+                                    onChange={(e) => handleTriggerValue(e, 'value')}
+                                    name="key"
+                                >
+                                    {valueOptions && valueOptions.map((actionOption) => (
+                                        <MenuItem key={actionOption.name} value={actionOption.value}>
+                                            {actionOption.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        ) : (
+                            actionType === "COLOR" ? (
+                                <input
+                                    type="color"
+                                    value={value}
+                                    onChange={(e) => handleTriggerKey(e, 'data')}
+                                    style={{
+                                        width: '100%',
+                                        height: '50px',
+                                        border: "none",
+                                        cursor: "pointer",
+                                        background: "none",
+                                    }}
+                                />
+                            ) : (
+                                <TextField
+                                    size='small'
+                                    label="Value"
+                                    fullWidth
+                                    value={value}
+                                    onChange={(e) => handleTriggerKey(e, 'data')}
+                                    name="data"
+                                    sx={{marginBottom: 2}}
+                                />
+                            )
+                        )}
                         <TextField
                             size='small'
-                            label="Value"
+                            label="Execution Order"
+                            type="number"
                             fullWidth
-                            value={value}
-                            onChange={(e) => handleTriggerKey(e, 'data')}
-                            name="data"
+                            value={order}
+                            inputProps={{
+                                min: 0,
+                                max: 50,
+                                step: 1
+                            }}
+                            onChange={(e) => setOrder(parseInt(e.target.value) || 1)}
+                            sx={{marginBottom: 1}}
+                        />
+
+                        <TextField
+                            size='small'
+                            label="Delay (seconds)"
+                            type="number"
+                            fullWidth
+                            maxRows={5}
+                            inputProps={{
+                                min: 0,
+                                max: 30,
+                                step: 1
+                            }}
+                            value={delaySeconds}
+                            onChange={(e) => setDelaySeconds(parseInt(e.target.value) || 0)}
                             sx={{marginBottom: 2}}
                         />
-                    )
-                )}
-                <TextField
-                    size='small'
-                    label="Execution Order"
-                    type="number"
-                    fullWidth
-                    value={order}
-                    inputProps={{
-                        min: 0,
-                        max: 50,
-                        step: 1
-                    }}
-                    onChange={(e) => setOrder(parseInt(e.target.value) || 1)}
-                    sx={{marginBottom: 1}}
-                />
+                        Revert action
+                        <Switch
+                            onChange={(e) => handleTriggerKey(e, 'revert')}
+                            checked={revert}
+                        ></Switch>
+                    </div>
+                </AccordionDetails>
+            </Accordion>
 
-                <TextField
-                    size='small'
-                    label="Delay (seconds)"
-                    type="number"
-                    fullWidth
-                    maxRows={5}
-                    inputProps={{
-                        min: 0,
-                        max: 30,
-                        step: 1
-                    }}
-                    value={delaySeconds}
-                    onChange={(e) => setDelaySeconds(parseInt(e.target.value) || 0)}
-                    sx={{marginBottom: 2}}
-                />
-                Revert action
-                <Switch
-                    onChange={(e) => handleTriggerKey(e, 'revert')}
-                    checked={revert}
-                ></Switch>
-            </div>
-        </Card>
+        </div>
     );
 };
