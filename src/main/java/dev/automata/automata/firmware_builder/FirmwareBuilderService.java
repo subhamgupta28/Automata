5a7f64e0550f9fa232d7fdede7534726edac6a16
Subham gupta <67224573+subhamgupta28@users.noreply.github.com>
2026-07-17 02:03:01 +0530

    MAJOR UPDATE: v12.1.2 optimizing docker image
    Signed-off-by: Subham gupta <67224573+subhamgupta28@users.noreply.github.com>

---
+++ src/main/java/dev/automata/automata/firmware_builder/FirmwareBuilderService.java
@@ -0,0 +1,21 @@
+package dev.automata.automata.firmware_builder;
+
+import dev.automata.automata.service.MainService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FirmwareBuilderService {
+
+    private final FirmwareService firmwareService;
+    private final MainService mainService;
+
+    @Scheduled(fixedRate = 120000)
+    public void run() {
+//        var devices = mainService.getAllDevice();
+
+//        firmwareService.registerDevice();
+    }
+}
